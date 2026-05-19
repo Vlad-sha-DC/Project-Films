f1dff0b668df2f7de67a24d35dc2bf1a38169c1f
vlad-sha <v.shevchenko@dunice.net>
2026-05-19 17:52:51 +0300

    добавлена запись в хранилище,формирование списка и удаление

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let data = {};
-let listData = [];
+let listData;
 
 let ratingInput = document.getElementById('rating');
 let ratingValue = document.getElementById('rating-value');
@@ -11,25 +11,59 @@ function updateRating (value) {
 ratingInput.addEventListener('input', e => updateRating(e.target.value));
 
 function formData () {
-    let id = Math.floor(Math.random() * 10000);
-    let title = document.getElementById('name').value;
+    let title = document
+                .getElementById('name')
+                .value;
+    if (!title) {
+        alert('Напишите название фильма!')
+        return;
+    }
     if (typeof title !== 'string') {
         alert ('Название фильма должно быть строкой!')
     }
-    let year = Number(document.getElementById('date').value.replaceAll('-',''));
+    let year = Number(document
+                    .getElementById('date')
+                    .value
+                    .replaceAll('-',''));
+    if (!year) {
+        alert('Укажите год выпуска фильма!')
+        return;
+    }
     if (typeof year !== 'number' || Number.isNaN(year)) {
-        alert ('Год должен быть числом!')
+        alert('Год должен быть числом!')
     }
-    let genres = document.getElementById('genre').value.split(/,\s*/);
+    let genres = document
+                .getElementById('genre')
+                .value
+                .split(/,\s*/);
+    if (!genres[0]) {
+        alert('Напишите жанры фильма!')
+        return;
+    }
     if (!Array.isArray(genres) && genres.every(e => typeof e === 'string')) {
-        alert ('Жанры должны быть массивом строк!')
+        alert('Жанры должны быть массивом строк!')
     }
-    let rating = document.getElementById("rating").valueAsNumber;
+    let rating = document
+                .getElementById("rating")
+                .valueAsNumber;
+    if (!rating) {
+        alert('Укажите рейтинг фильма!')
+        return;
+    }
     if (typeof rating !== 'number' || Number.isNaN(rating)) {
-        alert ('Кажется все сломалось')
+        alert('Кажется все сломалось')
     }
-    let watched = Boolean(document.getElementById("viewed").checked);
-    let comment = document.getElementById("comment").value;
+    let watched = Boolean(document
+                        .getElementById("viewed")
+                        .checked);
+    let comment = document
+                .getElementById("comment")
+                .value;
+    if (!comment) {
+        alert('Напишите комментарий!')
+        return;
+    }
+    let id = Math.floor(Math.random() * 10000);
     let createdAt = new Date().toLocaleDateString('ru-RU');
     data = {
         id,
@@ -41,11 +75,44 @@ function formData () {
         comment,
         createdAt
     };
-    console.log(data);
+    listData.push(data);
+    return true;
+}
+
+function clearData () {
+    document.getElementById('name').value = '';
+    document.getElementById('date').value = '';
+    document.getElementById('genre').value = '';
+    document.getElementById('rating').value = '';
+    updateRating(0);
+    document.getElementById('viewed').checked = false;
+    document.getElementById('comment').value = '';
+    data = {};
 }
 
 function saveData () {
-    formData()
-    listData.push(data);
-    console.log(listData);
+    if(!formData()) {
+        return;
+    }
+    localStorage.setItem('storageList', JSON.stringify(listData));
+    setList(listData);
+    clearData();
+}
+
+(function loadData () {
+    listData = localStorage.getItem('storageList') ? JSON.parse(localStorage.getItem('storageList')) : [];
+    setList(listData);
+})()
+
+let deleteElement = function (id) {
+    listData = listData.filter(element => element.id !== id);
+    localStorage.setItem('storageList', JSON.stringify(listData));
+    setList(listData);
+}
+
+function setList (listData) {
+    const listContainer = document.getElementById('filmList');
+    listContainer.innerHTML = listData.map(element => {
+        return `<li id=${element.id}>${element.title}</li><button onclick="deleteElement(${element.id})">Удалить</button>`;
+    }).join('');
 }
